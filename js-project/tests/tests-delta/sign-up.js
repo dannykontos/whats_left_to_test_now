b7480317d4465bcfa610bf4ce6522bcb0b732afa
daniel kontos <kontos.daniel@gmail.com>
2019-06-29 14:43:57 +0300

    Split tests in two separate files

--- js-project/tests/tests-delta/sign-up.js
+++ js-project/tests/tests-delta/sign-up.js
@@ -1,17 +1,6 @@
 var page = require('../../pages/pages-delta');
 browser.ignoreSyncronization = true;
 
-describe('Delta Homepage tests: ', function() {
-    it('should navigate to flytap home page', function() {
-        page.getDeltaHomePage();
-        expect(browser.getTitle()).toEqual("Airline Tickets & Flights: Book Direct with Delta Air Lines - Official Site");
-    });
-    it('should click on "Submit" button without filling any fields and get an error', function() {
-        page.submit();
-        expect(page.getText()).toContain("To continue,please correct the");
-    });
-});
-
 describe('Sign Up tests', function() {
     it('should navigate to flytap home page', function() {
         page.getDeltaHomePage();
@@ -21,7 +10,7 @@ describe('Sign Up tests', function() {
         page.clickSignUpBtn();
         expect(browser.getTitle()).toEqual("Join SkyMiles® Loyalty Program : Delta Air Lines");
     });
-    it('should click Complete button (empty required fields) and display an error message', function() {
+    it("should click 'Complete' button without filling any fields, and display an error message", function() {
         page.clickCompleteBtn();
         expect(page.getErrorMessageOnSignUp()).toContain("correct the 19 items indicated.");
     });
